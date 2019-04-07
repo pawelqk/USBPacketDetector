e244d76ec994dfcf8e650bd3b9ac3337a83c7297
Paweł <Pawe@DESKTOP-VTRBT1B>
2019-04-07 16:11:28 +0200

    Fixed whitespaces

--- USBPacketDetector.vhd
+++ USBPacketDetector.vhd
@@ -1,6 +1,5 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
---use IEEE.NUMERIC_STD.ALL;
 
 entity USBPacketDetector is
     Port ( usb_in : in  STD_LOGIC;
@@ -15,11 +14,11 @@ architecture Behavioral of USBPacketDetector is
    signal state, next_state : state_type; 
    signal output_signal : std_logic;
 
-	signal counter_reset: std_logic;
-	signal counter_mod_5: natural range 0 to 4;
-	signal counter_mod_9: natural range 0 to 8;
-	
-	signal shift_reg: std_logic_vector(7 downto 0);
+   signal counter_reset: std_logic;
+   signal counter_mod_5: natural range 0 to 4;
+   signal counter_mod_9: natural range 0 to 8;
+
+   signal shift_reg: std_logic_vector(7 downto 0);
    signal i_state: std_logic_vector(2 downto 0) := "001";
 
 begin
@@ -34,7 +33,7 @@ begin
       end if;
    end process;
    
-	detected <= output_signal;
+   detected <= output_signal;
    o_state <= i_state;
  
    OUTPUT_DECODE: process (state)
@@ -67,65 +66,65 @@ begin
          when st2_read =>
             i_state <= "010";
             if counter_mod_9 = 8 then
-					next_state <= st3_check;
+               next_state <= st3_check;
             end if;
          when st3_check =>
             i_state <= "011";
-				if shift_reg = "01010100" then
-					next_state <= st4_accept;
-				else
-					next_state <= st1_idle;
-				end if;
+            if shift_reg = "01010100" then
+               next_state <= st4_accept;
+            else
+               next_state <= st1_idle;
+            end if;
          when st4_accept =>
             i_state <= "100";
          when others =>
             next_state <= st1_idle;
       end case;      
    end process;
-	
+
 -- LICZNIK MODULO 5
 
-	process (CLK) 
-	begin
-		if rising_edge(CLK) then
-			if counter_reset='1' then 
-				counter_mod_5 <= 0;
-			else
-				if counter_mod_5=4 then
-					counter_mod_5 <= 0;
-				else
-					counter_mod_5 <= counter_mod_5 + 1;
-				end if;
-			end if;
-		end if;
-	end process; 
+   process (CLK)
+   begin
+      if rising_edge(CLK) then
+         if counter_reset='1' then
+            counter_mod_5 <= 0;
+         else
+            if counter_mod_5=4 then
+               counter_mod_5 <= 0;
+            else
+               counter_mod_5 <= counter_mod_5 + 1;
+            end if;
+         end if;
+      end if;
+   end process;
 
 
 -- LICZNIK MODULO 9 
 
-	process (CLK, counter_mod_5) 
-	begin
-		if rising_edge(CLK) then
-			if counter_reset='1' then
-				counter_mod_9 <= 0;
-			elsif counter_mod_5=1 then
-				if counter_mod_9=8 then
-					counter_mod_9 <= 0;
-				else
-					counter_mod_9 <= counter_mod_9 + 1;
-				end if;
-			end if;
-		end if;
-	end process;
+   process (CLK, counter_mod_5) 
+   begin
+      if rising_edge(CLK) then
+         if counter_reset='1' then
+            counter_mod_9 <= 0;
+         elsif counter_mod_5=1 then
+            if counter_mod_9=8 then
+               counter_mod_9 <= 0;
+            else
+               counter_mod_9 <= counter_mod_9 + 1;
+            end if;
+         end if;
+      end if;
+   end process;
 
 -- REJESTR PRZESUWNY
 
-	process(CLK)
-	begin
-		if rising_edge(CLK) and counter_mod_5=1 then
-			shift_reg( 7 downto 0 ) <= shift_reg( 6 downto 0 ) & usb_in;
-		end if;
-	end process;
+   process(CLK)
+   begin
+      if rising_edge(CLK) and counter_mod_5=1 then
+         shift_reg( 7 downto 0 ) <= shift_reg( 6 downto 0 ) & usb_in;
+      end if;
+   end process;
 
 end Behavioral;
 
--- USBPacketDetectorTest.vhd
+++ USBPacketDetectorTest.vhd
@@ -1,17 +1,3 @@
--- Vhdl test bench created from schematic /home/ise/VM_SHARED/USB_UCISW/scheme.sch - Sun Mar 24 21:12:07 2019
---
--- Notes: 
--- 1) This testbench template has been automatically generated using types
--- std_logic and std_logic_vector for the ports of the unit under test.
--- Xilinx recommends that these types always be used for the top-level
--- I/O of a design in order to guarantee that the testbench will bind
--- correctly to the timing (post-route) simulation model.
--- 2) To use this template as your testbench, change the filename to any
--- name of your choice with the extension .vhd, and use the "Source->Add"
--- menu in Project Navigator to import the testbench. Then
--- edit the user defined section below, adding code to generate the 
--- stimulus for your design.
---
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
@@ -22,47 +8,41 @@ END scheme_scheme_sch_tb;
 ARCHITECTURE behavioral OF scheme_scheme_sch_tb IS 
 
    COMPONENT scheme
-   PORT( CLK_IN	:	IN	STD_LOGIC;
+   PORT( CLK_IN   :   IN   STD_LOGIC;
           rst : IN STD_LOGIC;
-          Clk_60MHz	:	OUT	STD_LOGIC; 
-          detected	:	OUT	STD_LOGIC; 
+          Clk_60MHz   :   OUT   STD_LOGIC;
+          detected   :   OUT   STD_LOGIC;
           o_state: OUT STD_LOGIC_VECTOR(2 downto 0);
-          usb_in	:	IN	STD_LOGIC);
+          usb_in   :   IN   STD_LOGIC);
    END COMPONENT;
 
-   SIGNAL CLK_IN	:	STD_LOGIC := '0';
-   SIGNAL Clk_60MHz	:	STD_LOGIC;
-   SIGNAL detected	:	STD_LOGIC;
-   SIGNAL usb_in	:	STD_LOGIC;
+   SIGNAL CLK_IN   :   STD_LOGIC := '0';
+   SIGNAL Clk_60MHz   :   STD_LOGIC;
+   SIGNAL detected   :   STD_LOGIC;
+   SIGNAL usb_in   :   STD_LOGIC;
    SIGNAL o_state : STD_LOGIC_VECTOR(2 downto 0);
-	signal rst : STD_LOGIC := '0';
+   signal rst : STD_LOGIC := '0';
 
 BEGIN
 
    UUT: scheme PORT MAP(
-		CLK_IN => CLK_IN,
+      CLK_IN => CLK_IN,
       rst => rst,
-		Clk_60MHz => Clk_60MHz, 
-		detected => detected, 
-		usb_in => usb_in,
+      Clk_60MHz => Clk_60MHz, 
+      detected => detected, 
+      usb_in => usb_in,
       o_state => o_state
    );
 
    CLK_IN <= not CLK_IN after 10 ns; -- after 1 us / 120
--- *** Test Bench - User Defined Section ***
+
    tb : PROCESS
       constant test_bits : std_logic_vector (0 to 39) := B"1111_01010111_1111_01010100_0101010001010101";
    BEGIN
-		for i in test_bits'range loop
-			usb_in <= test_bits(i);
+      for i in test_bits'range loop
+         usb_in <= test_bits(i);
          wait for 1 us / 12;
---			wait until rising_edge(Clk_60MHz);
---			wait until rising_edge(Clk_60MHz);
---			wait until rising_edge(Clk_60MHz);
---			wait until rising_edge(Clk_60MHz);
---			wait until rising_edge(Clk_60MHz);
-		end loop;
+      end loop;
    END PROCESS;
--- *** End Test Bench - User Defined Section ***
 
 END;
