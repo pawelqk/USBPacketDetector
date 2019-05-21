0cf8bcc89a4e16e0b3909441d1f94ba7565a1eb0
Paweł <Pawe@DESKTOP-VTRBT1B>
2019-05-21 08:52:03 +0200

    Refactored VHDL code

--- USBPacketDetector.vhd
+++ USBPacketDetector.vhd
@@ -2,39 +2,40 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity USBPacketDetector is
-    Port ( usb_in : in  STD_LOGIC;
+    Port ( data_plus : in  STD_LOGIC;
            data_minus: in STD_LOGIC;
            CLK : in  STD_LOGIC;
            reset: in STD_LOGIC;
-           o_state: out std_logic_vector(2 downto 0);
            data_out: out STD_LOGIC_VECTOR(7 downto 0);
            data_ready: out STD_LOGIC;
-           detected : out  STD_LOGIC);
+           eop_detected : out  STD_LOGIC);
 end USBPacketDetector;
 
 architecture Behavioral of USBPacketDetector is
-   type state_type is (st1_idle, st2_read, st3_check, st4_accept, st5_show, st6_newline); 
-   signal state, next_state : state_type; 
-   signal output_signal : std_logic;
-   signal i_state: std_logic_vector(2 downto 0) := "001";
+   type state_type is (st1_idle, st2_read, st3_check, st4_accept, st5_show, st6_eop); 
+   signal state, next_state : state_type;
 
-   signal counter_reset: std_logic;
+   signal counter_reset: std_logic; -- flag for resetting counters
    signal counter_mod_5: natural range 0 to 4;
    signal counter_mod_9: natural range 0 to 8;
-   signal shift_reg: std_logic_vector(7 downto 0);
+   signal shift_reg: std_logic_vector(7 downto 0); -- register for detecting sync pattern
 
-   signal received_byte: std_logic_vector(7 downto 0);   -- container shift register for first received byte (least significant bit first)
-   signal output_byte: std_logic_vector(7 downto 0);
-   signal decoded_bit: std_logic;
-   signal previous_bit: std_logic;
-   signal in_data_ready: std_logic;
-   signal eop: std_logic;
-   signal se0: std_logic;
-   signal counter_ones: natural range 0 to 6;
-   signal unstuffed: std_logic;
-   signal detected_eop: std_logic;
+   signal received_byte: std_logic_vector(7 downto 0);   -- container shift register for received byte (least significant bit first)
+   signal output_byte: std_logic_vector(7 downto 0);     -- byte on output when data_ready = '1'
+   signal decoded_bit: std_logic;   -- decoded nrzi bit
+   signal previous_bit: std_logic;  -- previous bit for nrzi decoding
+   signal in_data_ready: std_logic; -- signals when byte is ready
+   signal se0_twice: std_logic;     -- flag for se0 twice in a row
+   signal se0: std_logic;           -- flag for se0 (data_plus = data_minus = '0') detected
+   signal counter_ones: natural range 0 to 6; -- counter for nrzi ones
+   signal detected_eop: std_logic;            -- signals eop (when se0_twice and data_plus = '1')
 
 begin
+   data_ready <= in_data_ready;
+   decoded_bit <= previous_bit xnor data_plus;
+   data_out <= output_byte;
+
+   -- resetting machine
    SYNC_PROC: process (CLK)
    begin
       if rising_edge(CLK) then
@@ -46,11 +47,7 @@ begin
       end if;
    end process;
 
-   data_ready <= in_data_ready;
-   decoded_bit <= previous_bit xnor usb_in;
-   data_out <= output_byte;
-   o_state <= i_state;
-   
+   -- showing byte on output when it's ready
    SHOW_BYTE: process (state, received_byte)
    begin
       if state = st5_show then
@@ -61,16 +58,9 @@ begin
          in_data_ready <= '0';
       end if;
    end process;
- 
-   OUTPUT_DECODE: process (state)
-   begin
-      if state = st4_accept then 
-         output_signal <= '1';
-      else
-         output_signal <= '0';
-      end if;
-   end process;
    
+   -- counting only when it's needed
+   -- during sync pattern accepting and byte reading
    COUNTER_STATE: process (state)
    begin
       if state = st2_read or state = st4_accept then
@@ -80,57 +70,51 @@ begin
      end if;
    end process;
    
+   -- signaling end of packet
    NEWLINE: process (state)
    begin
-      if state = st6_newline then 
-         detected <= '1';
+      if state = st6_eop then 
+         eop_detected <= '1';
       else
-         detected <= '0';
+         eop_detected <= '0';
       end if;
    end process;
 
-   NEXT_STATE_DECODE: process (state, usb_in, counter_mod_9, shift_reg, eop)
+   -- fsm process
+   NEXT_STATE_DECODE: process (state, data_plus, counter_mod_9, shift_reg, detected_eop)
    begin
       next_state <= state;
       case (state) is
          when st1_idle =>
-            i_state <= "001";
-            if usb_in = '0' then
+            if data_plus = '0' then
                next_state <= st2_read;
             end if;
          when st2_read =>
-            i_state <= "010";
             if counter_mod_9 = 8 then
                next_state <= st3_check;
             end if;
          when st3_check =>
-            i_state <= "011";
             if shift_reg = "01010100" then
                next_state <= st4_accept;
             else
                next_state <= st1_idle;
             end if;
          when st4_accept =>
-            i_state <= "100";
             if detected_eop = '1' then
-               next_state <= st6_newline;
+               next_state <= st6_eop;
             elsif counter_mod_9 = 8 then
                next_state <= st5_show;
             end if;
          when st5_show =>
-            i_state <= "101";
             next_state <= st4_accept;
-         when st6_newline =>
-            i_state <= "110";
+         when st6_eop =>
             next_state <= st1_idle;  
          when others =>
-            i_state <= "000";
             next_state <= st1_idle;
       end case;      
    end process;
 
--- LICZNIK MODULO 5
-
+   -- counter modulo 5
    process (CLK)
    begin
       if rising_edge(CLK) then
@@ -146,9 +130,7 @@ begin
       end if;
    end process;
 
-
--- LICZNIK MODULO 9 
-
+   -- counter modulo 9
    process (CLK, counter_mod_5, decoded_bit, counter_ones) 
    begin
       if rising_edge(CLK) then
@@ -164,28 +146,28 @@ begin
       end if;
    end process;
 
--- REJESTR PRZESUWNY
-
+   -- 8-bit shift register used during detecting sync pattern
    process(CLK, counter_mod_5)
    begin
       if rising_edge(CLK) and counter_mod_5=1 then
-         shift_reg( 7 downto 0 ) <= shift_reg( 6 downto 0 ) & usb_in;
+         shift_reg( 7 downto 0 ) <= shift_reg( 6 downto 0 ) & data_plus;
       end if;
    end process;
    
--- BIT DECODER
-   process(CLK, usb_in)
+   -- remembering previous state on data_plus
+   -- for further nrzi decoding
+   process(CLK, data_plus)
    begin
       if rising_edge(CLK) and counter_mod_5=1 then
-         previous_bit <= usb_in;
+         previous_bit <= data_plus;
       end if;
    end process;
  
---SE0 DETECTOR
-   process(CLK, usb_in, data_minus, counter_mod_5)
+   -- detecting se0 state
+   process(CLK, data_plus, data_minus, counter_mod_5)
    begin
       if rising_edge(CLK) and counter_mod_5=1 then
-         if (usb_in = '0') and (data_minus = '0') then
+         if (data_plus = '0') and (data_minus = '0') then
             se0 <= '1';
          else
             se0 <= '0';
@@ -193,24 +175,23 @@ begin
       end if;
    end process;
  
- 
--- 2x SE0 DETECTOR
+   -- detecting 2x se0 in a row
    process(CLK, se0, counter_mod_5)
    begin
       if rising_edge(CLK) and counter_mod_5=1 then
-         if (usb_in = '0') and (data_minus = '0') and (se0 = '1') then
-            eop <= '1';
+         if (data_plus = '0') and (data_minus = '0') and (se0 = '1') then
+            se0_twice <= '1';
          else
-            eop <= '0';
+            se0_twice <= '0';
          end if;
       end if;
    end process;
 
--- EOP DETECTOR
+   -- detecting end of packet
    process(CLK, se0, counter_mod_5)
    begin
       if rising_edge(CLK) and counter_mod_5=1 then
-         if (usb_in = '1') and (data_minus = '0') and (eop = '1') then
+         if (data_plus = '1') and (data_minus = '0') and (se0_twice = '1') then
             detected_eop <= '1';
          else
             detected_eop <= '0';
@@ -218,9 +199,9 @@ begin
       end if;
    end process;
 
-            
-
--- BYTE RECORDER
+   -- remembering the byte in 8-bit shift register
+   -- bit unstuffing is performed
+   -- zero after six consecutive ones is ignored
    process(CLK, counter_mod_5, decoded_bit, counter_ones, counter_mod_9)
    begin
       if rising_edge(CLK) and counter_mod_5=1 and state = st4_accept then
@@ -237,8 +218,6 @@ begin
             end loop;            
             received_byte(7) <= decoded_bit;
          end if;
-      elsif rising_edge(CLK) then
-         unstuffed <= '0';
       end if;
    end process;
 
--- USBPacketDetectorTest.vhd
+++ USBPacketDetectorTest.vhd
@@ -10,32 +10,21 @@ ARCHITECTURE behavioral OF scheme_scheme_sch_tb IS
    COMPONENT scheme
    PORT( CLK_IN   :   IN   STD_LOGIC;
           rst : IN STD_LOGIC;
-          Clk_60MHz   :   OUT   STD_LOGIC;
-          detected   :   OUT   STD_LOGIC;
-          o_state: OUT STD_LOGIC_VECTOR(2 downto 0);
-          usb_in   :   IN   STD_LOGIC;
+          data_plus   :   IN   STD_LOGIC;
           data_minus: IN STD_LOGIC);
    END COMPONENT;
 
    SIGNAL CLK_IN   :   STD_LOGIC := '0';
-   SIGNAL Clk_60MHz   :   STD_LOGIC;
-   SIGNAL detected   :   STD_LOGIC;
-   SIGNAL usb_in   :   STD_LOGIC;
-   SIGNAL o_state : STD_LOGIC_VECTOR(2 downto 0);
+   SIGNAL data_plus   :   STD_LOGIC;
    signal rst : STD_LOGIC := '0';
-   signal data_out: STD_LOGIC_VECTOR(7 downto 0);
    signal data_minus: std_logic;
-   signal data_ready: std_logic;
 
 BEGIN
 
    UUT: scheme PORT MAP(
       CLK_IN => CLK_IN,
       rst => rst,
-      Clk_60MHz => Clk_60MHz, 
-      detected => detected, 
-      usb_in => usb_in,
-      o_state => o_state,
+      data_plus => data_plus,
       data_minus => data_minus
    );
 
@@ -46,19 +35,10 @@ BEGIN
    BEGIN
       wait for 230 ns;
       for i in test_bits'range loop
-         usb_in <= test_bits(i);
+         data_plus <= test_bits(i);
          data_minus <= not test_bits(i);
          wait for 1 us / 12;
       end loop;
-      usb_in <= '0';
-      data_minus <= '0';
-      wait for 1 us / 12;
-      usb_in <= '0';
-      data_minus <= '0';
-      wait for 1 us / 12;
-      usb_in <= '1';
-      data_minus <= '0';
-      wait for 1 us / 12;
    END PROCESS;
 
 END;
